7c323ce6f56668138da2d372017d354e3409a377
yoann <yoann@LAPTOP-5JKAFLLK.localdomain>
2021-02-03 15:45:56 +0100

    add slugify service

---
+++ src/Service/Slugify.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Service;
+
+class Slugify
+{
+    /**
+     * @param $text
+     * @return string|string[]|null
+     */
+    function cleanString($text)
+    {
+        $utf8 = array(
+            '/[áàâãªä]/u'   =>   'a',
+            '/[ÁÀÂÃÄ]/u'    =>   'A',
+            '/[ÍÌÎÏ]/u'     =>   'I',
+            '/[íìîï]/u'     =>   'i',
+            '/[éèêë]/u'     =>   'e',
+            '/[ÉÈÊË]/u'     =>   'E',
+            '/[óòôõºö]/u'   =>   'o',
+            '/[ÓÒÔÕÖ]/u'    =>   'O',
+            '/[úùûü]/u'     =>   'u',
+            '/[ÚÙÛÜ]/u'     =>   'U',
+            '/ç/'           =>   'c',
+            '/Ç/'           =>   'C',
+            '/ñ/'           =>   'n',
+            '/Ñ/'           =>   'N',
+            '/\./'          =>   ' ',
+            '/–/'           =>   ' ', // UTF-8 hyphen to "normal" hyphen
+            '/[’‘‹›‚]/u'    =>   ' ', // Literally a single quote
+            '/[“”«»„]/u'    =>   ' ', // Double quote
+            '/ /'           =>   ' ', // nonbreaking space (equiv. to 0x160)
+        );
+        return preg_replace(array_keys($utf8), array_values($utf8), $text);
+    }
+
+    /**
+     * @param string $input
+     * @return string
+     */
+    public function generate(string $input) : string
+    {
+        return strtolower(
+            preg_replace(
+                array('#[\\s-]+#', '#[^A-Za-z0-9. -]+#'),
+                array('-', ''),
+                $this->cleanString(urldecode($input))
+            )
+        );
+    }
+}
