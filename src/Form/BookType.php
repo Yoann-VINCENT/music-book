9ec563cf3bce0eb019d1efdd58797d7db8f2cd58
yoann <yoann@LAPTOP-5JKAFLLK.localdomain>
2021-02-03 16:01:18 +0100

    Fixtures + CRUD

---
+++ src/Form/BookType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Book;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('cover')
+            ->add('created_at')
+            ->add('slug')
+            ->add('author')
+            ->add('category')
+            ->add('fav_users')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Book::class,
+        ]);
+    }
+}
