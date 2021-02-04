419ed4e9a55d6744c6b8afcc4af74a617c916f86
yoann <yoann@LAPTOP-5JKAFLLK.localdomain>
2021-02-04 17:09:54 +0100

    js ok, music plays !

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,7 @@ Encore
      * and one CSS file (e.g. app.scss) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('music', './assets/js/music.js')
     //.addEntry('page1', './assets/page1.js')
     //.addEntry('page2', './assets/page2.js')
 
