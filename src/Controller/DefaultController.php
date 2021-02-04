40fdf23118f755f985206d67c3d504ec82de0a0b
yoann <yoann@LAPTOP-5JKAFLLK.localdomain>
2021-02-04 10:30:23 +0100

    display books on home

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 // src/Controller/DefaultController.php
 namespace App\Controller;
 
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,9 +11,14 @@ class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
+     * @param BookRepository $bookRepository
+     * @return Response
      */
-    public function index(): Response
+    public function index(BookRepository $bookRepository): Response
     {
-        return $this->render('default/index.html.twig');
+        $books = $bookRepository->findAll();
+        return $this->render('default/index.html.twig', [
+            'books' => $books,
+        ]);
     }
 }
